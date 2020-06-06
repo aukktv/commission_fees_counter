1b424e7fc7b6a247e20ec1ff340cc4daae5a1f8e
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-06-06 19:09:17 +0300

    Main functionality added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const UserInfo = require('./models/userInfo')
+const app = express()
+
+//objektas hendlebars konfiguravimui
+const hbs = exphbs.create({
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+})
+
+//hbs modulis registruojamas expresse kaip irankis html psl atvaizdavimui
+app.engine('hbs', hbs.engine)
+
+//po registracijos dar butina nurodyti du param, cia jis jau naudojamas
+app.set('view engine', 'hbs')
+app.set('views', 'pages')
+
+//uzregistruojame kataloga public kaip viesa, t.y rasant failo varda su /priekyje, express zino, kad jis
+//yra siame kataloge (pvz '/style.css') - nereikia nurodyti viso kelio
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+   res.render('index')
+})
+
+app.post('/', async (req, res) =>{
+    const userInfo = new UserInfo(req.body.id, req.body.amount, req.body.user, req.body.operation)
+    await userInfo.save()
+    res.redirect('/result')
+    
+})
+
+app.get('/result', async (req, res) => {
+const userInfo = await UserInfo.getAll()
+const size = userInfo.length
+const last = userInfo[size-1]
+
+console.log(last)
+    res.render('result', {last})
+  
+ })
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ models/userInfo.js
@@ -0,0 +1,89 @@
+const fs = require('fs')
+const path = require('path')
+const dateAndTime = new Date()
+const date = dateAndTime.toLocaleDateString()
+
+class UserInfo{
+    constructor(amount, user, operation){
+        this.user = user
+        this.operation = operation
+        this.amount = amount
+        this.id = Date.now()
+        this.date = date
+        this.currency = "EUR"
+
+    }
+
+  resultIn(){
+     if(this.operation === "cash_in" && this.amount * 0.0003 <= 5){
+        return Math.ceil((this.amount*0.0003)*100)/100
+     }else{
+         return 5
+     }
+  }
+
+  resultOut(){
+       if(this.user === "juridical" && this.amount<=1.67){
+         return 0.5
+        }else if (this.user === "natural" && this.amount>1000){
+            return Math.ceil(((this.amount - 1000)*0.003)*100)/100
+        }else if (this.user === "natural" && this.amount<=1000){
+            return 0
+     }else{
+         return Math.ceil((this.amount*0.003)*100)/100
+     }
+  }
+
+    toJSON(){
+        return ({
+            date: this.date,
+            user_id: this.id,
+            user_type: this.user,
+            type: this.operation,
+            operation: {
+                amount: this.amount,
+                currency: this.currency
+            },
+            result: this.operation === "cash_in"?this.resultIn():this.resultOut()
+        })
+    }
+
+    //metodas sudes duomenis i JSOn ir saugos atskirame faile
+    async save(){
+        const userinfo = await UserInfo.getAll()
+        userinfo.push(this.toJSON())
+
+        return new Promise((resolve, reject) =>{
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'userInfo.json'),
+                JSON.stringify(userinfo),
+                (err) => {
+                    if(err){
+                        reject(err)
+                    }else{
+                        resolve()
+                    }
+                }
+            )
+        })
+    }
+
+    static getAll(){
+        return new Promise((resolve, reject) =>{
+            fs.readFile(
+                path.join(__dirname, '..' , 'data', 'userInfo.json'),
+                'utf-8',
+                (err, content) =>{
+                    if(err) {
+                        reject(err)
+                    } else {
+                        resolve(JSON.parse(content))
+                        //resolve(content)
+                    }
+                }
+            )
+        }) 
+    }
+}
+
+module.exports = UserInfo
